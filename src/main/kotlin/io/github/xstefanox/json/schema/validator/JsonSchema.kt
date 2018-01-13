157ab511d1d1c6b870ac8c2b9e8e35df84c4db10
Stefano Varesi <stefano.varesi@gmail.com>
2018-01-13 11:29:34 +0100

    added json schema media type

--- src/main/kotlin/io/github/xstefanox/json/schema/validator/JsonSchema.kt
+++ src/main/kotlin/io/github/xstefanox/json/schema/validator/JsonSchema.kt
@@ -1,3 +1,13 @@
 package io.github.xstefanox.json.schema.validator
 
-abstract class JsonSchema
+abstract class JsonSchema {
+
+    companion object {
+
+        /**
+         * The Media Type for a JSON Schema as defined in [rfc6838](https://tools.ietf.org/html/rfc6838).
+         */
+        @JvmField
+        val MEDIA_TYPE = "application/schema+json"
+    }
+}
